72d3256549f1ed3783b087212c6c0c8689bcb261
Javonne Johnson <jjavonne13@gmail.com>
2025-08-07 23:31:57 -0400

    Initial Commit

---
+++ timeTableQuizApp/timeTableQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeTableQuizAppApp.swift
+//  timeTableQuizApp
+//
+//  Created by Javonne Johnson on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct timeTableQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
